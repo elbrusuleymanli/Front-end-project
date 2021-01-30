ef7e5ea9fe737b05d4a4b4b81651a5be096da58b
elbrusuleymanli <elbrusis@code.edu.az>
2021-01-31 00:56:48 +0400

    update

---
+++ assets/JS/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+$(".left").click(function(){
+$(".active-content-area").fadeIn();
+$(this).css("background","blue")
+// $("#login").addClass(".show");
+
+    
+ $("#sign-up").fadeOut(".show");
+
+});
+
+});
