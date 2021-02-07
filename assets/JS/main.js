4d964d8d49b4707988836667a7109f71c9feb3c1
elbrusuleymanli <elbrusis@code.edu.az>
2021-02-07 23:44:27 +0400

    almost finish

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,125 +1,140 @@
 $(document).ready(function () {
-// login modal login
-$(".left").click(function(){
+  // login modal login
+  $(".left").click(function () {
     $(".sign-up-content").hide();
     $(".active-content-area").show();
-    $(this).css({"background":"blue","color":"white","font-weight":"bold"});
-    $(".right").removeAttr('style');
-    
-});
-// login modal sign up
-$(".right").click(function(){
+    $(this).css({ background: "blue", color: "white", "font-weight": "bold" });
+    $(".right").removeAttr("style");
+  });
+  // login modal sign up
+  $(".right").click(function () {
     $(".active-content-area").hide();
-    $(".left").removeAttr('style');
+    $(".left").removeAttr("style");
     $(".sign-up-content").show();
-    $(this).css({"background":"blue","color":"white","font-weight":"bold"});
-    $(".left").css({"background":"none","color":"black"});
-   
-    });
-       //faq
-    $(".nav-item").click(function(){
-      
-      $(this).css({"background":"blue"});
-      $('a').attr("color","white");
-    })
+    $(this).css({ background: "blue", color: "white", "font-weight": "bold" });
+    $(".left").css({ background: "none", color: "black" });
+  });
+  //faq
+  $(".nav-item").click(function () {
+    $(this).css({ background: "blue" });
+    $("a").attr("color", "white");
+  });
+  // mob-menu
 
+  $(".mobile-btn").click(function () {
+    $("body").addClass("show-mobile-menu");
+  });
 
-    // owl carousel plugin for part of categories
-    $(".categories-slider").owlCarousel({
-        loop: true,
+  $(".mob-close").click(function () {
+    $("body").removeClass("show-mobile-menu");
+  });
+
+  $(".overlay").click(function (e) {
+    if (e.target == this) {
+      $("body").removeClass("show-mobile-menu");
+    }
+  });
+
+  $(".nav-dropdown").click(function (e) {
+    e.preventDefault();
+    $(e.target).parents(".dropdown").find(".dropdown-menu").slideToggle();
+  });
+
+  // owl carousel plugin for part of categories
+  $(".categories-slider").owlCarousel({
+    loop: true,
+    items: 3,
+    dots: true,
+    margin: 30,
+    responsive: {
+      0: { items: 1 },
+      768: {
+        items: 2,
+      },
+      1200: {
         items: 3,
-        dots: true,
-        margin: 30,
-        responsive: {
-          0: { items: 1 },
-          768: {
-            items: 2,
-          },
-          1200: {
-            items: 3,
-          },
-        },
-      });
-    // owl carousel plugin for part of testimonials
-      $(".owl-card-body ").owlCarousel({
-        loop: true,
-        // items: 4,
-        dots: true,
-        margin: 30,
-        responsive: {
-          0: { items: 1 },
-          705: {
-            items: 2,
-          },
-          1200: {
-            items: 4,
-          },
-        },
-      });
-    
-    // owl carousel plugin for part of about
-      $(".about-owl-card").owlCarousel({
-        loop: true,
-       
-        dots: true,
-        margin: 30,
-        responsive: {
-          0: { items: 1 },
-          705: {
-            items: 2,
-          },
-          1200: {
-            items: 3,
-          },
-        },
-      });
-      // owl carousel plugin for part of latest news 
-      $(".lates-news-slider").owlCarousel({
-        loop: true,
+      },
+    },
+  });
+  // owl carousel plugin for part of testimonials
+  $(".owl-card-body ").owlCarousel({
+    loop: true,
+    // items: 4,
+    dots: true,
+    margin: 30,
+    responsive: {
+      0: { items: 1 },
+      705: {
+        items: 2,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
+
+  // owl carousel plugin for part of about
+  $(".about-owl-card").owlCarousel({
+    loop: true,
+
+    dots: true,
+    margin: 30,
+    responsive: {
+      0: { items: 1 },
+      705: {
+        items: 2,
+      },
+      1200: {
         items: 3,
-        dots: true,
-        margin: 30,
-        responsive: {
-          0: { items: 1 },
-          705: {
-            items: 2,
-          },
-          1200: {
-            items: 3,
-          },
-        },
-      });
-           //plagun for select options in banner page of index
-      $("select").niceSelect();
-          
-      //scroll to up
-      $(window).on("scroll", function() {
-        if ($(this).scrollTop() > 70) {
-          $('.scroll-up').fadeIn();
-        } else {
-          $('.scroll-up').fadeOut();
-        }
-      });
-   
+      },
+    },
+  });
+  // owl carousel plugin for part of latest news
+  $(".lates-news-slider").owlCarousel({
+    loop: true,
+    items: 3,
+    dots: true,
+    margin: 30,
+    responsive: {
+      0: { items: 1 },
+      705: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+  //plagun for select options in banner page of index
+  $("select").niceSelect();
 
-      $('.scroll-up').on("click", function()  {
-        $("html, body").animate({
-          scrollTop: 0
-        }, 100,"swing");
-        return false;
-      });
-     
-     
-      //navbar fixed
-     
-      $(window).scroll(function (e){
+  //scroll to up
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 70) {
+      $(".scroll-up").fadeIn();
+    } else {
+      $(".scroll-up").fadeOut();
+    }
+  });
 
-        if($(this).scrollTop()>84){
-          $(".header-main").addClass("header-fixed");
-        }
-        else{
-          $(".header-main").removeClass("header-fixed");
-        }
-      })
-			
+  $(".scroll-up").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      100,
+      "swing"
+    );
+    return false;
+  });
+
+  //navbar fixed
+
+  $(window).scroll(function (e) {
+    if ($(this).scrollTop() > 84) {
+      $(".header-main").addClass("header-fixed");
+    } else {
+      $(".header-main").removeClass("header-fixed");
+    }
+  });
 });
